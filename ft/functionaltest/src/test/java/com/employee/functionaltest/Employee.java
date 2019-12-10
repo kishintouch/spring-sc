f8e1d98d664ae4b61bbc564b1e74a9b845904e40
Kishore Kumar R <kishore.kumar.r@ericsson.com>
2019-12-10 07:54:11 +0530

    Rest Assured Funtional Test Case

--- ft/functionaltest/src/test/java/com/employee/functionaltest/Employee.java
+++ ft/functionaltest/src/test/java/com/employee/functionaltest/Employee.java
@@ -2,6 +2,33 @@ package com.employee.functionaltest;
 
 public class Employee {
 	String name;
+	int salary;
+	int age;
+	int id;
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
--- ft/functionaltest/src/test/java/com/employee/functionaltest/Insertemployee.java
+++ ft/functionaltest/src/test/java/com/employee/functionaltest/Insertemployee.java
@@ -1,14 +1,17 @@
 package com.employee.functionaltest;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.OutputStream;
 import java.io.IOException;
-
+import java.io.FileOutputStream;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.json.*;
 import org.apache.commons.io.IOUtils;
@@ -18,51 +21,77 @@ import com.google.gson.JsonObject;
 
 public class Insertemployee {
 
-	
-	@Test
-	public  void insertdetails() throws IOException
-	{
+	@BeforeClass
+	public static void setup() {
+
 		RestAssured.baseURI = "http://dummy.restapiexample.com";
 
-		// Get the RequestSpecification of the request that you want to sent
-		// to the server. The server is specified by the BaseURI that we have
-		// specified in the above step.
+	}
+
+	//@Test
+	public void insertEmployee() throws IOException {
+
 		RequestSpecification httpRequest = RestAssured.given();
-		
-		FileInputStream fileInputStream = new FileInputStream(new File (".//jsonInput//test.json") );
-		//create json objects to send the parameters
-		/*JSONObject requestParams = new JSONObject();
-		
-		requestParams.put("name",  "ameren");
-		requestParams.put("salary", 45121);
-		requestParams.put("age", 27); */
-			
-		httpRequest.header("Content-Type","application/json").accept("application/json");
-		httpRequest.body(IOUtils.toString(fileInputStream,"UTF-8"));
-		Response response=httpRequest.post("/api/v1/create");
-		System.out.println(response.contentType());
-		
-		
-		
-		//Employee emp = response.getBody().as(Employee.class);
-		//System.out.println("Deseriliation of json to java object " + emp.getName());
+		FileInputStream fileInputStream = new FileInputStream(new File(".//TestData//AddEmployee.json"));
+
+		httpRequest.header("Content-Type", "application/json").accept("application/json");
+		httpRequest.body(IOUtils.toString(fileInputStream, "UTF-8"));
+		Response response = httpRequest.post("/api/v1/create");
+
 		String responseBody = response.getBody().asString();
-		System.out.println("Response Body is =>  " + responseBody);
-		
-		
+
+		// Deseriliation of json to java object
 		Gson gson = new Gson();
 		Employee emp = gson.fromJson(responseBody, Employee.class);
-		
-		
-		
+
 		System.out.println(emp.getName());
-		System.out.println("Response Body is =>  " + responseBody);
 
 		int statusCode = response.getStatusCode();
-		System.out.println(statusCode);
 		Assert.assertEquals(statusCode, 200);
-	//	String successCode = response.jsonPath().get("SuccessCode");
-	//	Assert.assertEquals( "Correct Success code was returned", successCode, "OPERATION_SUCCESS");
+
+		// Serialize the data as Json and Save it as Json
+
+		String saveEmployee = new Gson().toJson(emp);
+		OutputStream outputStream = new FileOutputStream(".//TestData//SavedEmployee.json");
+		outputStream.write(saveEmployee.getBytes());
+		outputStream.close();
+
+		// GET Request
+		FileInputStream inputStream = new FileInputStream(new File(".//TestData//SavedEmployee.json"));
+		String getEmployee = IOUtils.toString(inputStream, "UTF-8");
+		Employee getEmployeeObject = gson.fromJson(getEmployee, Employee.class);
+
+		Response getResponse = httpRequest.pathParam("id", getEmployeeObject.getId()).get("/api/v1/create");
+		Employee getEmployeeResponse = gson.fromJson(getResponse.getBody().asString(), Employee.class);
+		Assert.assertEquals("Employee Name is Incorrect", getEmployeeResponse.getName(), "jodi");
+
+		// PUT Request
+		/*Employee updateEmployee = new Employee();
+		updateEmployee.setAge(34);
+		updateEmployee.setName("Testtt");
+		
+		httpRequest.header("Content-Type", "application/json").accept("application/json");
+		httpRequest.body(new Gson().toJson(updateEmployee));
+		Response putResponse = httpRequest.pathParam("id", getEmployeeObject.getId()).get("/api/v1/create");
+		
+		updateEmployee = gson.fromJson(putResponse.getBody().asString(), Employee.class);
+		System.out.println("updated Employee " + updateEmployee.getSalary());
+		Assert.assertEquals("Employee Name is Incorrect", updateEmployee.getSalary(), null); */
+	}
+	
+
+	@Test
+	public void getEmployee() throws IOException {
+
+		RequestSpecification httpRequest = RestAssured.given();
+
+		// GET Request
+		httpRequest.header("Content-Type", "application/json").accept("application/json");
+		Response getResponse = httpRequest.basePath("/api/v1/employee").get("/{id}",137247);
+		Gson gson = new Gson();
+		System.out.println("getResponse.getBody().asString() " + getResponse.getBody().asString());
+		Employee getEmployeeResponse = gson.fromJson(getResponse.getBody().asString(), Employee.class);
+		Assert.assertEquals("Employee Name is Incorrect", getEmployeeResponse.getName(), "kodi");
 
 	}
 
