13ab16106f7e74504a9c6645197d98fc63230402
Kishore Kumar R <kishore.kumar.r@ericsson.com>
2020-06-29 13:04:39 +0530

    adding conditional sorting

--- shopping-cart-api/src/main/java/com/ecommerce/cart/comparator/CartPriceCompare.java
+++ shopping-cart-api/src/main/java/com/ecommerce/cart/comparator/CartPriceCompare.java
@@ -12,6 +12,10 @@ public class CartPriceCompare implements Comparator<CartProductModel>
 	@Override
 	public int compare(CartProductModel o1, CartProductModel o2) {
 		int res =  o1.getProductPrice().compareTo(o2.getProductPrice());
+		//If the Both Prices are equal then compare and sort it with product name
+		if(res == 0) {
+			res = o1.getProductName().compareTo(o2.getProductName());
+		}
 		System.out.println("res " + res);
 		 return res;
 	} 
